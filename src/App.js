64770bec9eeacc75922dfa7b84f28434c64fe8e7
sanjai kumar n <nsksanjai@gmail.com>
2022-02-03 21:20:23 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { TextField, Typography, Button } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+   <Typography variant='h1'>Form</Typography>
+   
+      
+    </form>
   );
 }
 
