1c46cd0561a080b10bb352f0518f6a814ae8119f
sanjai kumar n <nsksanjai@gmail.com>
2022-02-23 20:20:28 +0530

    3333

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from "react";
 import Form from "./component/Form";
-<<<<<<< HEAD
 
-=======
->>>>>>> 93b52b42c3cd0836df80e3022d45d8dfccbf7c0d
 
 function App() {
     return(
     <Form />
-<<<<<<< HEAD
-  )
-=======
-    
-
-    )
->>>>>>> 93b52b42c3cd0836df80e3022d45d8dfccbf7c0d
-};
-
+    )};
 export default App;
