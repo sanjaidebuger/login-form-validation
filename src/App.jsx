1ef9e9088289c2b8a063982db2711fa01004282b
sanjai kumar n <nsksanjai@gmail.com>
2022-02-23 13:16:46 +0530

    aaa

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function App() {
+    return(
+    <form action="post">
+        <label>
+            Name:
+            <input type="submit" value="submit" />
+        </label>
+    </form>
+
+    )
+};
+
+export default App;
